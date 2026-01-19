fb2ad690c6296c4f286385c568297e976a1da96f
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2026-01-19 15:43:20 -0500

    A2 Snapshot 3 (Final Snapshot)

--- Prog4A2/Assets/Scripts/Action Tasks/UpdateGrade.cs
+++ Prog4A2/Assets/Scripts/Action Tasks/UpdateGrade.cs
@@ -1,14 +1,20 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
-
+using UnityEngine;
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class UpdateGrade : ActionTask {
 
+		public BBParameter<GameObject> followBox;
+		public Blackboard myBlackboard;
+
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit() {
+
+			myBlackboard = agent.GetComponent<Blackboard>();
+
 			return null;
 		}
 
@@ -16,7 +22,42 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			EndAction(true);
+
+            Blackboard cubeBoard = followBox.value.GetComponent<Blackboard>();
+            float finalLapTime = myBlackboard.GetVariableValue<float>("lapTime");
+
+			if(finalLapTime < 3f)
+			{
+
+				cubeBoard.SetVariableValue("letterGrade", "A");
+
+			}else if (finalLapTime < 5) {
+
+                cubeBoard.SetVariableValue("letterGrade", "B");
+
+            }
+            else if (finalLapTime < 7)
+			{
+
+                cubeBoard.SetVariableValue("letterGrade", "C");
+
+            }
+            else if (finalLapTime < 9)
+			{
+
+                cubeBoard.SetVariableValue("letterGrade", "D");
+
+            } else
+			{
+
+                cubeBoard.SetVariableValue("letterGrade", "F");
+
+            }
+
+			myBlackboard.SetVariableValue("lapTime", 0f);
+			cubeBoard.SetVariableValue("waypointsHit", 0);
+            EndAction(true);
+
 		}
 
 		//Called once per frame while the action is active.
